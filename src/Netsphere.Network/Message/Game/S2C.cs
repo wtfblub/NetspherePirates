ef4d9e6cee32a2ef3ab2ce710682f14c97957db0
wtfblub <ecx@hmamail.com>
2017-01-09 16:14:08 +0100

    Items are now equipable on first login fixed #18

--- src/Netsphere.Network/Message/Game/S2C.cs
+++ src/Netsphere.Network/Message/Game/S2C.cs
@@ -231,15 +231,19 @@ namespace Netsphere.Network.Message.Game
         public CharacterStyle Style { get; set; }
 
         [BlubMember(2)]
-        public byte Unk1 { get; set; }
+        public byte MaxSkills { get; set; }
 
         [BlubMember(3)]
-        public byte Unk2 { get; set; }
+        public byte MaxWeapons { get; set; }
 
         public SSuccessCreateCharacterAckMessage()
-        { }
+        {
+            MaxSkills = 1;
+            MaxWeapons = 3;
+        }
 
         public SSuccessCreateCharacterAckMessage(byte slot, CharacterStyle style)
+            : this()
         {
             Slot = slot;
             Style = style;
