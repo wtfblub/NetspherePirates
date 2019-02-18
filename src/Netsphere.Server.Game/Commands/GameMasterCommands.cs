b079ad67eb4bbe0f3f32f63d19e873d5496cbd6b
wtfblub <wtfblub@gmail.com>
2019-02-18 09:23:32 +0100

    Add announce command

--- src/Netsphere.Server.Game/Commands/GameMasterCommands.cs
+++ src/Netsphere.Server.Game/Commands/GameMasterCommands.cs
@@ -5,6 +5,13 @@ namespace Netsphere.Server.Game.Commands
 {
     public class GameMasterCommands : ICommandHandler
     {
+        private readonly PlayerManager _playerManager;
+
+        public GameMasterCommands(PlayerManager playerManager)
+        {
+            _playerManager = playerManager;
+        }
+
         [Command(
             CommandUsage.Player,
             SecurityLevel.GameMaster,
@@ -14,6 +21,22 @@ namespace Netsphere.Server.Game.Commands
         {
             plr.IsInGMMode = !plr.IsInGMMode;
             plr.SendConsoleMessage(plr.IsInGMMode ? "You are now in GM mode" : "You are no longer in GM mode");
+            return true;
+        }
+
+        [Command(
+            CommandUsage.Player,
+            SecurityLevel.GameMaster,
+            "Usage: announce"
+        )]
+        public async Task<bool> Announce(Player plr, string[] args)
+        {
+            if (args.Length == 0)
+                return false;
+
+            foreach (var p in _playerManager)
+                p.SendNotice(string.Join("", args));
+
             return true;
         }
     }
