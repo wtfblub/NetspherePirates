07af1790ff8668ed4d9b740b1819910bdb5e97af
wtfblub <wtfblub@gmail.com>
2019-04-26 17:28:44 +0200

    Add pagination for ItemsView

--- src/tools/Netsphere.Tools.ShopEditor/ViewModels/ItemsViewModel.cs
+++ src/tools/Netsphere.Tools.ShopEditor/ViewModels/ItemsViewModel.cs
@@ -1,22 +1,63 @@
 ﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
+using Avalonia;
+using Netsphere.Tools.ShopEditor.Models;
 using Netsphere.Tools.ShopEditor.Services;
 using Netsphere.Tools.ShopEditor.Views;
+using Reactive.Bindings;
 using ReactiveUI;
+using ReactiveUI.Legacy;
+using ReactiveCommand = ReactiveUI.ReactiveCommand;
 
 namespace Netsphere.Tools.ShopEditor.ViewModels
 {
     public class ItemsViewModel : TabViewModel
     {
+        private const int ItemsPerPage = 20;
+        private readonly ObservableAsPropertyHelper<string> _pageString;
+
         public override string Header => "Items";
 
-        public ShopService ShopService { get; }
         public ReactiveCommand AddItem { get; }
+        public ReactiveCommand NextPage { get; }
+        public ReactiveCommand PrevPage { get; }
+        public IReactiveList<ShopItem> Items { get; }
+        public ReactiveProperty<int> CurrentPage { get; }
+        public ReactiveProperty<int> PageCount { get; }
+        public string PageString => _pageString.Value;
 
         public ItemsViewModel()
         {
-            ShopService = ShopService.Instance;
             AddItem = ReactiveCommand.CreateFromTask(AddItemImpl);
+            Items = new ReactiveList<ShopItem>();
+            CurrentPage = new ReactiveProperty<int>(1);
+            PageCount = new ReactiveProperty<int>(ShopService.Instance.Items.Count);
+
+            ShopService.Instance.Items.Changed.Subscribe(_ => PageCount.Value = ShopService.Instance.Items.Count);
+            _pageString = this.WhenAnyValue(x => x.CurrentPage.Value, x => x.PageCount.Value)
+                .Select(x => $"{x.Item1} / {x.Item2}")
+                .ToProperty(this, x => x.PageString);
+
+            this.WhenAnyValue(x => x.PageCount.Value)
+                .Subscribe(x =>
+                {
+                    if (CurrentPage.Value > x)
+                        CurrentPage.Value = x;
+
+                    Items.Clear();
+                    Items.AddRange(ShopService.Instance.Items.Skip(ItemsPerPage * (CurrentPage.Value - 1)).Take(ItemsPerPage));
+                });
+
+            var canNextPage = this.WhenAnyValue(x => x.CurrentPage.Value, x => x.PageCount.Value)
+                .Select(x => x.Item1 < x.Item2);
+
+            var canPrevPage = this.WhenAnyValue(x => x.CurrentPage.Value)
+                .Select(x => x > 1);
+
+            NextPage = ReactiveCommand.CreateFromTask(NextPageImpl, canNextPage);
+            PrevPage = ReactiveCommand.CreateFromTask(PrevPageImpl, canPrevPage);
         }
 
         private async Task AddItemImpl()
@@ -32,6 +73,20 @@ namespace Netsphere.Tools.ShopEditor.ViewModels
             {
                 await new MessageView("Error", "Unable to add item", ex).ShowDialog(Application.Current.MainWindow);
             }
+        }
+
+        private async Task NextPageImpl()
+        {
+            CurrentPage.Value++;
+            Items.Clear();
+            Items.AddRange(ShopService.Instance.Items.Skip(ItemsPerPage * (CurrentPage.Value - 1)).Take(ItemsPerPage));
+        }
+
+        private async Task PrevPageImpl()
+        {
+            CurrentPage.Value--;
+            Items.Clear();
+            Items.AddRange(ShopService.Instance.Items.Skip(ItemsPerPage * (CurrentPage.Value - 1)).Take(ItemsPerPage));
         }
     }
 }
